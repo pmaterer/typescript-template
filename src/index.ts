8b3e089f921a808ddbbee460e8c8498296c3fe93
Patrick Materer <patrickmaterer@gmail.com>
2021-06-15 09:33:18 -0500

    Adds a more interesting template

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,12 @@ export function ping(): string {
     return 'pong';
 }
 
-console.log('This place is not a place of honor…no highly esteemed deed is commemorated here… nothing valued is here.');
+function msg(): void {
+    const app = document.getElementById('app');
+    const h2 = document.createElement('h2');
+    h2.textContent =
+        'This place is not a place of honor…no highly esteemed deed is commemorated here… nothing valued is here.';
+    app?.appendChild(h2);
+}
+
+msg();
